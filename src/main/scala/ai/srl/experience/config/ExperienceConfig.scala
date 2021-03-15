1a059a637d81ae4da797d7488a6ab3edc52ef814
mpskowron@gmail.com <mpskowron@gmail.com>
2021-03-15 19:45:43 +0100

    Moved ExperienceConfig Description instance to its companion object for easier importing

--- src/main/scala/ai/srl/experience/config/ExperienceConfig.scala
+++ src/main/scala/ai/srl/experience/config/ExperienceConfig.scala
@@ -9,12 +9,13 @@ case class ReplayConfig(batchSize: Int, bufferSize: Int, prioritised: PriorityCo
 
 case class PriorityConfig(defaultPriority: Float)
 
-given Description[ExperienceConfig] with
-  extension (config: ExperienceConfig) 
-    def describe() =
-      import config._
-      Seq(
-        ("minBufferSize", minBufferSize.toString),
-        ("batchSize", replay.batchSize.toString),
-        ("bufferSize", replay.bufferSize.toString),
-      )
+object ExperienceConfig:
+  given Description[ExperienceConfig] with
+    extension (config: ExperienceConfig) 
+      def describe() =
+        import config._
+        Seq(
+          ("minBufferSize", minBufferSize.toString),
+          ("batchSize", replay.batchSize.toString),
+          ("bufferSize", replay.bufferSize.toString),
+        )
