1f12ac5703daf5546bdc4d27c3820b1a42aaf0e4
mpskowron@gmail.com <mpskowron@gmail.com>
2021-04-03 19:30:59 +0200

    Changed Reward to type class

--- src/main/scala/ai/srl/reward/Reward.scala
+++ src/main/scala/ai/srl/reward/Reward.scala
@@ -3,22 +3,23 @@ package ai.srl.reward
 import ai.srl.action.DJLAction
 import ai.srl.env.RlEnv
 
-trait Reward[Ac, E <: RlEnv[Ac, ?, ?]]:
-  /**
-   * @param env Assummed to be in a state after executing an action
-   * @param action Action which led environment ot its current state
-   * @return
-   */
-  def reward(env: E, action: Ac): Double
+trait Reward[R, Ac, E <: RlEnv[Ac, ?, ?]]:
+  extension (r: R)
+    /**
+     * @param env Assummed to be in a state after executing an action
+     * @param action Action which led environment ot its current state
+     * @return
+     */
+    def reward(env: E, action: Ac): Double
 
-  /**
-   * @param env
-   * @return false if this is the last step of env in which reward method can be invoked, true otherwise
-   */
-  def isDone(env: E): Boolean
+    /**
+     * @param env
+     * @return false if this is the last step of env in which reward method can be invoked, true otherwise
+     */
+    def isDone(env: E): Boolean
 
-  /**
-   * Needs to be called as a part of env's reset method
-   * @param env
-   */
-  def reset(env: E): Unit
+    /**
+     * Needs to be called as a part of env's reset method
+     * @param env
+     */
+    def reset(env: E): Unit
