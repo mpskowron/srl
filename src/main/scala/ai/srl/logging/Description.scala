6ea636bbed949a9eeb02699fbacebcce6633329f
michal <mpskowron@gmail.com>
2023-04-02 22:22:16 +0200

    feat(description): added method which extends description by current date time

--- src/main/scala/ai/srl/logging/Description.scala
+++ src/main/scala/ai/srl/logging/Description.scala
@@ -2,13 +2,18 @@ package ai.srl.logging
 
 import java.io.FileWriter
 import java.nio.file.Path
-
 import org.supercsv.io.CsvListWriter
 import org.supercsv.prefs.CsvPreference
+import zio.ZIO
+import Description.given
+
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 trait Description[A]:
   extension (d: A) def describe(): Seq[(String, String)]
-  extension (d: A) def save(path: Path) = 
+  extension (d: A) def save(path: Path) =
     val (header, row) = d.describe().unzip
     val file = path.toFile
     file.getParentFile().mkdirs()
@@ -16,6 +21,13 @@ trait Description[A]:
     if file.length == 0 then listWriter.writeHeader(header: _*)
     listWriter.write(row: _*)
     listWriter.close()
+
+  extension (d: A) def saveWithDateTime(path: Path): ZIO[Any, Throwable, Unit] =
+    for
+      dateTimeDesc <- Description.currentDateTime
+      combinedDescription = d.describe() ++ Seq(dateTimeDesc, ("",""))
+      _ <- ZIO.attempt(combinedDescription.save(path))
+    yield ()
 
 class Descriptable[A: Description](val a: A):
   def describe(): Seq[(String, String)] = a.describe()
@@ -32,3 +44,9 @@ object Description:
   
   given Description[List[Descriptable[?]]] with
     extension (descs: List[Descriptable[?]]) def describe() = descs.flatMap(_.describe())
+
+  def currentDateTime: ZIO[Any, Nothing, (String, String)] =
+    for {
+      clock <- ZIO.clock
+      time <- clock.currentDateTime
+    } yield ("dateTime", time.withOffsetSameInstant(ZoneOffset.UTC).truncatedTo(ChronoUnit.MINUTES).toString)
